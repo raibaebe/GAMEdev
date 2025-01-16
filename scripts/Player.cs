36b33a27a020d94a71f71e224e1deee440960a9f
ALHNk <88340494+ALHNk@users.noreply.github.com>
2025-01-16 14:07:26 +0500

    SnowMAn and Player

---
+++ scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private float moveInput, verInput;
+	private Rigidbody2D rb;
+	public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    move();
+    }
+	public void move(){
+		moveInput = Input.GetAxis("Horizontal");
+		verInput = Input.GetAxis("Vertical");
+		rb.velocity = new Vector2(moveInput * speed, verInput*speed);
+		//anim.Play(walkAnim);
+	}
+
+}
